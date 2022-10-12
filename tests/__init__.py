2c41e3fbedc315899cdbb94feab6b4dad18483b2
c0fec0de <c0fec0de@gmail.com>
2022-10-12 06:56:52 +0200

    Remove endline whitespaces.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of Git Workspace.
 #
 # Git Workspace is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free 
+# the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 #
@@ -13,4 +13,3 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with Git Workspace. If not, see <https://www.gnu.org/licenses/>.
-
